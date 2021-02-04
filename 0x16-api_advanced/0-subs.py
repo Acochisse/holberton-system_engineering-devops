6c133a4a93f6afb6339f3c8ffef9ef47f30661a2
1819@holbertonschool.com <1819@holbertonschool.com>
2021-02-04 16:45:05 -0600

    changed sub string format

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,10 +6,10 @@ import requests as r
 
 
 def number_of_subscribers(subreddit):
-    headers = {'User-Agent': 'Blake'}
-    endpoint = 'https://reddit.com/r/{}/about.json'
+    headers = {'User-Agent': 'user'}
+    endpoint = 'https://reddit.com/r/{}/about.json'.format(subreddit)
 
-    sub = r.get(endpoint.format(subreddit), headers=headers)
+    sub = r.get(endpoint, headers=headers, allow_redirects=False)
 
     if subs.status_code is not 200:
         return 0
