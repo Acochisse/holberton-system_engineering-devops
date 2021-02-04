845dcec638f86034b1c8e4c277fcfd5890e7f49e
1819@holbertonschool.com <1819@holbertonschool.com>
2021-02-04 16:49:44 -0600

    changed subs to sub

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ def number_of_subscribers(subreddit):
     headers = {'User-Agent': 'user'}
     endpoint = 'https://reddit.com/r/{}/about.json'.format(subreddit)
 
-    sub = r.get(endpoint, headers=headers, allow_redirects=False)
+    subs = r.get(endpoint, headers=headers, allow_redirects=False)
 
     if subs.status_code is not 200:
         return 0
