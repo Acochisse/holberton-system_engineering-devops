529c1e114fd0ba23028724e78b618eb4ef33eb30
1819@holbertonschool.com <1819@holbertonschool.com>
2021-02-04 17:03:30 -0600

    made changes from 0

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,7 +8,7 @@ def top_ten(subreddit):
     """prints the titles of the first 10 hot posts listed"""
 
     endpoint = "https://www.reddit.com/r/{}.json?sort=hot&libit=10".format(subreddit)
-    headers = {'user-Agent': 'user'}
+    headers = {'user-Agent': 'blake'}
     subs = r.get(endpoint, headers=headers, allow_redirects=False)
 
     if subs.status_code != 200:
