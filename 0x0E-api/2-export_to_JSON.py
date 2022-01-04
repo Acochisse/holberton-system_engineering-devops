0d4ba4e108d17ff677136c620c7cd237eb73afe3
1819@holbertonschool.com <1819@holbertonschool.com>
2022-01-04 15:39:04 -0600

    pep8

--- 0x0E-api/2-export_to_JSON.py
+++ 0x0E-api/2-export_to_JSON.py
@@ -27,7 +27,6 @@ if __name__ == "__main__":
         todo['username'] = key.get('username')
         todos.append(todo)
 
-
         dictionary = {argv[1]: todos}
 
         with open('{}.json'.format(argv[1]), mode='w') as jsonfile:
