30b3a8345c56bd33041ce8722331f718a93be963
1819@holbertonschool.com <1819@holbertonschool.com>
2022-01-04 13:25:15 -0600

    adding task 3

---
+++ 0x0E-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""returns a dictionary of lists of dictionaries"""
+import csv
+import json
+import requests
+
+
+users_data = requests.get(
+    'https://jsonplaceholder.typicode.com/users').json()
+
+dictionary = {}
+
+for key in users_data:
+    new_todos = []
+    todo_list = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId={}'.
+        format(key.get('id'))).json()
+
+    for key2 in todo_list:
+        todo = {}
+        todo['task'] = key2.get('title')
+        todo['completed'] = key2.get('completed')
+        todo['username'] = key.get('username')
+        new_todos.append(todo)
+    dictionary[key.get('id')] = new_todos
+
+with open('todo_all_employees.json', mode='w') as jsonfile:
+    json.dump(dictionary, jsonfile)
