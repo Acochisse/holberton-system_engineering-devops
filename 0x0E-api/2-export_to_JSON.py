5c131c84eb09919774078ce1af19982ed86d4975
1819@holbertonschool.com <1819@holbertonschool.com>
2022-01-04 13:02:53 -0600

    adding task 2

---
+++ 0x0E-api/2-export_to_JSON.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" export api data to JSON """
+import csv
+import json
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    users_data = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.
+        format(argv[1])).json()
+
+    todo_list = requests.get(
+        'https://jsonplaceholder.typicode.com/usersId={}'.
+        format(argv[1])).json()
+
+    employee_name = users_data['name']
+    completed_tasks = 0
+    total_tasks = 0
+
+
+    todos = []
+    for key in todo_list:
+        todo = {}
+        todo['task'] = key.get('title')
+        todo['completed'] = key.get('completed')
+        todo['username'] = key.get('username')
+        todos.append(todo)
+
+
+        dictionary = {argv[1]: todos}
+
+        with open('{}.json'.format(argv[1]), mode='w') as jsonfile:
+            json.dumps(dictionary, json)
