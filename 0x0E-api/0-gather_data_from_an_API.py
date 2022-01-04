01125711b65abab5f686ef192e34b8f56b2fd076
Acochisse <1819@holbertonschool.com>
2022-01-04 09:28:10 -0600

    adding task zero, fix /t

---
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""using given REST API:
+https://jsonplaceholder.typicode.com/
+for a given employee ID,
+returns information about his/her TODO list progress.
+"""
+import json
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+        users_data = requests.get(
+                    'https://jsonplaceholder.typicode.com/users/{}'.
+                    format(argv[1]))
+
+        todo_list = requests.get(
+                    'https://jsonplaceholder.typicode.com/todos?userId={}'.
+                    format(argv[1]))
+
+        dict_users = users_data.json()
+        dict_todos = todo_list.json()
+        employee_name = dict_users['name']
+        done_tasks = []
+        completed_tasks = 0
+        total_tasks = 0
+
+        for key in dict_todos:
+            if key.get('completed') is True:
+                completed_tasks += 1
+                done_tasks.append(key.get('title'))
+            total_tasks += 1
+
+        print("Employee {} is done with tasks({}/{})".
+              format(employee_name,
+                     completed_tasks,
+                     total_tasks))
+
+        for key in done_tasks:
+            print("/t {}".format(key))
